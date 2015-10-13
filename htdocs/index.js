f8cedef2c23d4ad48f22f2dc4aa1eda517d6b2e6
Tetsutaro Endo <tetsu9taro25@gmail.com>
2015-10-14 02:29:03 +0900

    made task area

--- htdocs/index.js
+++ htdocs/index.js
@@ -16,6 +16,24 @@ if (Meteor.isClient) {
   Meteor.subscribe("tasks");
 
   Template.body.helpers({
+    date : function () {
+      var today = new Date();
+      var year = today.getFullYear();
+      var month = today.getMonth()+1;
+      var week = today.getDay();
+      var day = today.getDate();
+      var day_of_the_week = new Array("Sun","Mon","Tue","Wed","Thu","Fri","Sat");
+      return year + "/" + month + "/" + day + " " + day_of_the_week[week];
+    },
+    hours: [
+      { text: "09" },{ text: 10 },{ text: 11 },{ text: 12 },{ text: 13 },{ text: 14 },{ text: 15 },{ text: 16 },{ text: 17 },{ text: 18 },{ text: 19 },{ text: 20 },{ text: 21 },{ text: 22 },{ text: 23 }
+    ],
+    bars: [
+      { text: "09" },{ text: 10 },{ text: 11 },{ text: 12 },{ text: 13 },{ text: 14 },{ text: 15 },{ text: 16 },{ text: 17 },{ text: 18 },{ text: 19 },{ teext: 20 },{ text: 21 },{ text: 22 },{ text: 23 }
+    ],
+    projects: [
+      { text: "09" },{ text: 10 },{ text: 11 },{ text: 12 },{ text: 13 },{ text: 14 },{ text: 15 },{ text: 16 },{ text: 17 },{ text: 18 },{ text: 19 },{ text: 20 },{ text: 21 },{ text: 22 },{ text: 23 }
+    ],
     tasks: function () {
       if (Session.get("hideCompleted")) {
         return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
@@ -32,8 +50,12 @@ if (Meteor.isClient) {
     "submit .new-task": function (event) {
       event.preventDefault();
       var text = event.target.text.value;
-      Meteor.call("addTask", text);
+      var planstime = event.target.planstime.value;
+      var planetime = event.target.planetime.value;
+      Meteor.call("addTask", text, planstime, planetime);
       event.target.text.value = "";
+      event.target.planstime.value = "";
+      event.target.planetime.value = "";
     },
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
@@ -64,12 +86,14 @@ if (Meteor.isClient) {
 }
 
 Meteor.methods({
-  addTask: function (text) {
+  addTask: function (text , planstime, planetime) {
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
     Tasks.insert({
       text: text,
+      planstime: planstime,
+      planetime: planetime,
       createdAt: new Date(),
       owner: Meteor.userId(),
       username: Meteor.user().username
